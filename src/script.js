54019ba61c4a43e5744a9a7d5caab9ad0859d511
susene <susanne.eneroth@centimedia.se>
2020-05-16 14:47:54 +0200

    first commit

---
+++ src/script.js
@@ -0,0 +1,22 @@
+$('.content').slick({
+  dots: true,
+  // vertical: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  prevArrow: false,
+  nextArrow: false,
+  dotsClass: "vertical-dots"
+});
+
+$('.content_2').slick({
+  dots: true,
+  // vertical: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  prevArrow: false,
+  nextArrow: false
+});
